d8b201fcbbf64fd193d2d114f0df85d86f6c0765
Mseely-owner <mseely.owner@freakybwc420.com>
2024-08-09 14:45:37 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+// setTimeout(function () {
+//   window.location = "https://zoom.us/appdir";
+// }, 25);
+//window.location = "zoom://"
+
+export default function App() {
+  return <a href="https://zoom.us/j/89415358616?from=join#success">Link</a>;
+}
